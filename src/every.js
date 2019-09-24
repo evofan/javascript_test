e538f576d10175ded0f52c7fdb15e965ddc960bb
evofan <evofan@outlook.jp>
2019-09-24 21:59:08 +0900

    add file

---
+++ src/every.js
@@ -0,0 +1,96 @@
+﻿// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/every
+// Array.prototype.every()
+
+// every() メソッドは、与えられた関数によって実行されるテストに、「配列のすべての要素」が「通るかどうか ＝ true or false」をテストします。
+// 注: このメソッドは空の配列ではあらゆる条件式に対して true を返します。
+
+let array1 = [1, 30, 39, 29, 10, 13];
+
+function isBelowThreshold(currentValue) {
+  return currentValue < 40;
+}
+
+console.log(array1.every(isBelowThreshold)); // true
+
+// Syntax
+// arr.every(callback[, thisArg])
+//
+// callback()は3つの引数を取る（element...現在の要素、index...現在にindex、対象の配列）
+// thisArgはcallbackを実行する時にthisとして使用されるオブジェクト
+// 戻り値：コールバック関数が配列のすべての要素について truthy 値を返した場合は true。それ以外は false。
+
+console.log("");
+
+// 例：全ての配列要素の大きさをテストする
+// 配列内の全ての要素が10より大きいかどうかテストする
+function isBigEnough(element, index, array) {
+  return element >= 10;
+}
+console.log("全部10より大きい？：", [12, 5, 8, 130, 44].every(isBigEnough)); // false
+console.log("全部10より大きい？：", [12, 54, 18, 130, 44].every(isBigEnough)); // true
+
+// アロー関数ver.
+console.log("全部10より大きい？：", [12, 5, 8, 130, 44].every(x => x >= 10)); // false
+console.log("全部10より大きい？：", [12, 54, 18, 130, 44].every(x => x >= 10)); // true
+
+console.log("");
+
+// Polyfill ... every()が存在しないブラザウでは↓をスクリプトの先頭に入れる事でevery()と同等の内容を実行出来る（考え方）
+
+if (!Array.prototype.every) {
+  Array.prototype.every = function(callbackfn, thisArg) {
+    "use strict";
+
+    var T, k;
+
+    if (this == null) {
+      throw new TypeError("this is null or not defined");
+    }
+
+    // [1] O は、this を引数として ToObject に渡し、実行した結果です。
+    var O = Object(this);
+
+    // [2] lenValue は、"length" を引数として O の Get 内部メソッドを実行した結果です。
+    // [3] len を ToUint32(lenValue) とします。
+    var len = O.length >>> 0;
+
+    // [4] IsCallable(callbackfn) が false の場合、TypeError 例外がスローされます。
+    if (typeof callbackfn !== "function") {
+      throw new TypeError();
+    }
+
+    // [5] thisArg が与えられた場合、T は thisArg となり、さもなくば T は undefined となります。
+    if (arguments.length > 1) {
+      T = thisArg; // self = this的なもの
+    }
+
+    // [6] k を 0 とします。
+    k = 0;
+
+    // [7] k < len が成り立つ間、繰り返します。
+    while (k < len) {
+      var kValue;
+
+      // [a] Pk を ToString(k) とします。
+      //     これは in 演算子の左オペランドについて暗黙的です。
+      // [b] kPresent は、Pk を引数として O の HasProperty 内部メソッドを実行した結果です。
+      //     このステップは c と組み合わせられます。
+      // [c] kPresent が true の場合、続きます。
+      if (k in O) {
+        // [i] kValue は、Pk を引数として O の Get 内部メソッドを実行した結果です。
+        kValue = O[k];
+
+        // [ii] testResult は、this 値としての T と、kValue、k、0 を含む引数リストを
+        //      ともなって、callbackfn の Call 内部メソッドを実行した結果です。
+        var testResult = callbackfn.call(T, kValue, k, O);
+
+        // [iii] ToBoolean(testResult) が false の場合、false を返します。
+        if (!testResult) {
+          return false;
+        }
+      }
+      k++;
+    }
+    return true;
+  };
+}
---
+++ src/find.js
@@ -0,0 +1,119 @@
+﻿// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+// Array.prototype.find()
+
+// find() メソッドは配列の要素に指定されたテスト関数を適用していき、テストを満たす「最初の要素」の「値」を返します。
+// 見付からない場合は undefined を返します。
+
+// 配列の中から該当する値を返す
+let array1 = [5, 12, 8, 130, 44];
+let found1 = array1.find(function(elment) {
+  return elment > 10;
+});
+console.log("found1: ", found1); // found1:  12、★最初の値なので、12を返して終了
+
+let found2 = array1.find(function(elment) {
+  return elment > 200;
+});
+console.log("found2: ", found2); // found2:  undefined、★見つからない場合はundefinedを返して終了
+
+// Syntax
+// arr.find(callback[, thisArg])
+//
+// callback()は最大3つの引数を取る（element...現在の配列内の要素、現在の配列内のインデックス、findを呼び出した元の配列）
+// thisArg...callback()が実行される時にthisとして扱われるオブジェクト
+
+// 例：プロパティの1つによって配列内のオブジェクトを検索します
+let inventory = [
+  { name: "apples", quantity: 2 },
+  { name: "bananas", quantity: 0 },
+  { name: "cherries", quantity: 5 }
+];
+
+function isCherries(fruit) {
+  return fruit.name === "cherries";
+}
+
+console.log(inventory.find(isCherries)); // {name: "cherries", quantity: 5}
+
+// Using ES2015 arrow function
+const result = inventory.find(fruit => fruit.name === "cherries"); // 同じ物があった場合に返す例
+console.log("result", result); // Object { name: "cherries", quantity: 5 }
+
+// 例：配列内の素数を検索する、※prime number = 素数
+function isPrime(element, index, array) {
+  // ※コールバックが取る3つの引数
+  let start = 2;
+  while (start <= Math.sqrt(element)) {
+    // Math.sqrt = 平方根を求める
+    if (element % start++ < 1) {
+      return false;
+    }
+  }
+  return element > 1;
+}
+console.log("isPrime: ", [4, 6, 8, 12].find(isPrime)); // isPrime:  undefined
+console.log("isPrime: ", [4, 6, 5, 8, 12].find(isPrime)); // isPrime:  5
+
+console.log("");
+
+// 次の例は、存在しない要素と削除された要素が訪問され、コールバックに渡される値が訪問されたときの値である事を示しています。
+
+// インデックス2、3、4に要素のない配列を宣言します
+let array2 = [0, 1, , , , 5, 6];
+
+// 値が割り当てられたインデックスだけでなく、すべてのインデックスを表示します
+array2.find(function(value, index) {
+  console.log("訪問したインデックス：" + index + " と、その値：" + value);
+});
+/*
+    訪問したインデックス：0 と、その値：0
+    訪問したインデックス：1 と、その値：1
+    訪問したインデックス：2 と、その値：undefined
+    訪問したインデックス：3 と、その値：undefined
+    訪問したインデックス：4 と、その値：undefined
+    訪問したインデックス：5 と、その値：5
+    訪問したインデックス：6 と、その値：6
+*/
+
+console.log("");
+
+// 削除済みを含むすべてのインデックスを表示します
+array2.find(function(value, index) {
+  // 最初の反復で要素5を削除します
+  if (index === 0) {
+    console.log("値を持つarray2[5]を削除しています：" + array2[5]);
+    delete array2[5];
+  }
+  // エレメント5は、削除されたにもかかわらずまだ訪問されています
+  console.log("訪問したインデックス：" + index + " と、その値：" + value);
+});
+/*
+    値を持つarray2[5]を削除しています：5
+    訪問したインデックス：0 と、その値：0
+    訪問したインデックス：1 と、その値：1
+    訪問したインデックス：2 と、その値：undefined
+    訪問したインデックス：3 と、その値：undefined
+    訪問したインデックス：4 と、その値：undefined
+    訪問したインデックス：5 と、その値：undefined
+    訪問したインデックス：6 と、その値：6
+*/
+
+console.log("");
+
+console.log("自分サンプル：深い階層にある特定のデータを判別する");
+
+obj = {
+  name: "Silvia",
+  speed: 100,
+  break: false,
+  accel: true,
+  passenger: [{ wife: 1 }, { children: 2 }, { cat: 1 }],
+  year: 1997
+};
+let isCat = obj.passenger.find(element => element.cat);
+console.log("isCat: ", isCat); // isCat:  {cat: 1}
+let isDog = obj.passenger.find(element => element.dog);
+console.log("isDog: ", isDog); // isDog:  undefined
+
+console.log("");
+
