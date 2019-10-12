3e5b9d19b5550f0089a82033bfe76ec11b9c640d
evofan <evofan@outlook.jp>
2019-10-12 17:28:56 +0900

    add promise

---
+++ src/promise.js
@@ -0,0 +1,187 @@
+﻿// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Promise
+// Promise
+
+// Promise オブジェクトは非同期処理の最終的な完了処理（もしくは失敗）およびその結果の値を表現します。
+// ※ここではPromiseのコンストラクタと、そのようなオブジェクトとメソッド・プロパティを説明するので、
+// ※使用方法については「Promiseの使用」を読む
+// 通常コンストラクタは、Promiseに対応してない関数をラップする為に使われる
+
+let promise2 = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        resolve("fooo");
+    }, 300);
+});
+
+promise2.then(function(value) {
+    console.log(value);
+});
+
+console.log(promise2);
+// Promise {<pending>} ↓
+// Promise {[[PromiseStatus]]: "resolved", [[PromiseValue]]: "fooo"}
+// fooo
+
+
+// ■ Syntax
+// new Ptomise(executor)
+
+// 引数
+// executor ... 2つの引数resolveとrejectを取る関数
+// executorはresolve関数と、reject関数が渡されており、Promiseが実装されるとすぐに実行される。
+// （Promiseコンストラクタがオブジェクトと返すより早く実行される）
+
+// resove関数とreject関数が呼び出されると、Promiseに対してそれぞれresolve(解決)、reject(拒否)を行う))
+
+// executorは、通常、非同期の作業を開始して、完了した際にresolve関数（成功時）又はreject関数（エラー時）を呼び出す
+// executor関数でエラーが投げられた場合はrejectされる
+
+
+// ■ 解説
+// Promiseインターフェースは、作成時点では分からなくてもよい値へのプロキシである
+// Promiseを用いる事で、非同期のアクションに対し、成功や失敗のハンドラーを関連付ける事が出来る
+// これにより、非同期メソッドは未来のある時点で値を持つPromiseを返す＝同期メソッドと同じに扱える？
+
+// Promiseの状態は以下のいずれかになる
+// ・pending： 初期状態。成功も失敗もしていない
+// ・fulfilled： 処理が成功して完了した事を意味します
+// ・rejected： 処理が失敗した事を意味します
+
+// pending状態のPromiseは、何らかんも値を持つfulfilled状態、もしくは何らかの理由（エラー）を持つrejected状態に変わる
+// そのどちらになっても、then()メソッドによって関連付けられたハンドラーが呼ばれる
+// （対応するハンドラーがアタッチされた時、既にPromiseが成功又は失敗していても、そのハンドラーは呼ばれる＝よって
+//  非同期処理とその関連付けられたハンドラーとの競合は発生しない？）
+
+// Promise.prototype.then()メソッドと、Promise.prototype.catch()メソッドもまたPromiseを返すので、これらをチェーンする事が出来る。
+
+// 「チェーンの流れの図」
+
+
+// ■ プロパティ
+// Promise.length ... 長さを表し、常に１（コンストラクタの引数の数）
+// Ptomise.prototype ... Promiseコンストラクタのプロトタイプ
+
+
+// ■ メソッド
+// Promise.all(iterable) ... 全て（複数）のPromiseが解決されるか、拒否されるまで待つ。
+                             // 返却されたPromiseが「解決」なら、複数のPromiseが含まれるiterableで定義された通りの順番で入った集合知に値によって解決される
+                             // 拒否された場合は、iterableの中で最初に拒否されたPromiseの理由で拒否される
+
+// Promise.allSettled(iterable) ... 全て（複数）のPromiseが完了（解決or拒否）するまで待つ
+                                    // 結果はそれぞれのPromiseの結果を記述するオブジェクト配列で返される
+
+// Promise.race(iterable) ... Promiseの内、1つが解決又は拒否されるまで待つ。
+                              // 解決 → iterableの中で最初に解決されたPromiseの値によって解決される
+                              // 拒否 → 最初に拒否されたPromiseの理由によって拒否される
+
+// Promise.reject() ... 与えられた理由で拒否された新しいPromiseオブジェクトを返す
+
+// Promise.resolve() ... 与えられた値で解決された新しいPromiseオブジェクトを返す
+                        // もし値がiterable（つまりthenメソッドを持っている）ならば、返されるPromiseはそのthenrableを辿り、その結果を採用する
+                        // itrableでなければ、返されるPromiseは与えられた値で解決する
+                        // 一般的にある値がPromiseかどうか分からない場合は、Promise.resolve(value)を使ってPromiseにして扱います
+
+
+// ■ Promiseプロトタイプ
+
+// □ プロパティ
+// Promise.prototype.consutructor ... インスタンスのプロトタイプを生成した関数を返す。デフォルトはPromise関数
+
+// □ メソッド
+// Promise.prototype.catch(onRejected) ... プロミスに失敗ハンドラコールバックを付加する。呼ばれるとコールバックの戻り値、又は、新しいプロミスを返す
+
+// Promise.prototype.then(onFufilled) ... プロミスに成功ハンドラと失敗ハンドラを付加する。呼ばれたハンドラの戻り値によって解決している新しいプロミスを返す
+
+
+// ■ Promiseを作成する
+// Promiseオブジェクトはnewキーワードとコンストラクターで作成される。コンストラクターはexecutor関数と呼ばれる引数を取る。
+// executor関数は2つの関数を引数として取る。1つ目の関数（resolve）は成功時に呼ばれ、タスクの結果を値として返す。
+// 2つ目の関数（reject）はタスクが失敗した時に呼ばれ、失敗した理由（エラーオブジェクト）を返す
+const myFirstPromise = new Promise((resolve, reject) => {
+    // 最終的にいずれかを呼び出す非同期処理を実行します。
+    // resolve(someValue); // fulfilled
+    // 又は
+    // reject("failure reason"); // rejected
+});
+
+// Promise機能を持つ関数を作るには、単純にPromiseオブジェクトを返すようにする
+function myAsybcFunction(url) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.onLoad = () => resolve(xhr.responseText); // ※resolveを返す
+        xhr.onError = () => reject(xhr.statusText); // ※rejectを返す
+        xhr.send();
+    });
+}
+
+// ■ 例
+
+// □ 基本的な使用
+let myFirstPromise2 = new Promise((resolve, reject) => {
+    // 非同期で行っていたことが成功した場合にresolve（...）を呼び出し、失敗した場合にreject（...）を呼び出します。
+    // この例では、setTimeout（...）を使用して非同期コードをシミュレートします。
+    // 実際には、おそらくXHRまたはHTML5 APIのようなものを使用することになります。
+    setTimeout(function() {
+        resolve("Success!"); // わーい！ すべてが私たちに行きました
+    }, 1000);
+});
+
+myFirstPromise2.then((successMessage) => {
+    // successMessageは、上記のresolve（...）関数で渡したものです。
+    // 文字列である必要はありませんが、成功メッセージのみである場合は、おそらくそうです。
+    console.log("Yay! " + successMessage); // Yay! Success!
+});
+
+
+// □ 応用例
+// 以下の例はPromiseの仕組みを示したものです。
+// testPromiseメソッドは<button>をクリックする度に呼び出されます。
+// testPromiseメソッドは、Window.setTimeout()を用いて、1秒から3秒のランダムな時間の後、メソッドがこれまでに呼ばれた回数で成功するPromiseを作成する
+// Promise()コンストラクターはPromiseを作成する為に使用される
+
+// Promiseの成功は、p1.then()で設定されたコールバックによって記録される。
+// この記録から、メソッドの同期処理部分が、Promiseによる非同期処理からどのように分離されているか分かる
+// 'use strict';
+let promiseCount = 0;
+
+function testPromise() { // カウントを引数で渡さずglobalを参照でOK？
+    let thisPromiseCount = ++promiseCount;
+    
+    let log = document.getElementById("log");
+    log.insertAdjacentHTML("beforeend", thisPromiseCount + "）開始（<small>同期処理開始</small><br />）");
+
+    // 新しいPromiseを作成、1～3秒後に結果を返す
+    let p1 = new Promise(
+
+        // excutor関数はpromiseの成功又は失敗に応じて呼ばれる
+        (resolve ,reject) => {
+            log.insertAdjacentHTML("beforeend", thisPromiseCount + "）Promise開始（<small>非同期処理開始</small>）<br />");
+            
+            // 非同期を作成する為の一例です
+            window.setTimeout(
+                function() {
+                    // 約束を果たしました
+                    resolve(thisPromiseCount);
+                }, Math.random() * 2000 + 1000);
+        }
+    );
+
+    // Promiseが成功した時に何をするのかを決める
+    // then()で成功した時
+    // catch()で失敗した時
+    p1.then(
+        // メッセージと値を記録する
+        function(val) {
+           log.insertAdjacentHTML("beforeend", val + "）Promise成功（<small>非同期処理成功</small>）<br />"); 
+    }).catch(
+        // 失敗した理由を記録する
+        (reason) => {
+            console.log("Handle rejected promise（ " + reason + "）here.");
+    });
+
+    log.insertAdjacentHTML("beforeend", thisPromiseCount + "）Promiseは作成されました（<small>同期処理終了</small><br />）");
+
+}
+
+// ■ XHRによる画像の読み込み
+// https://github.com/mdn/js-examples/tree/master/promises-test
---
+++ src/promise_use.js
@@ -0,0 +1,281 @@
+﻿import { fail } from "assert";
+
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Guide/Using_promises
+// Promiseを使う
+
+// Promise は非同期処理の最終的な完了もしくは失敗を表すオブジェクトです。
+// 多くの人々は既存の用意されたPromiseを使う事になるため、このガイドでは、Promiseの作成方法の前に、関数が返すPromiseの使い方から説明します。
+
+// 本質的に、Promiseはコールバックを関数に渡すかわりに、関数が返したオブジェクトに対してコールバックを登録するようにする、というものです。
+
+// 例えば createAudioFileAsync()という非同期に音声ファイルを生成する関数を考えましょう。
+// この関数はコンフィグオブジェクトと2つのコールバック関数を受け取り、片方のコールバックは音声ファイルが無事作成されたときに呼び出され、
+// もう一つはエラーが発生したときに呼び出されます。
+
+// 以下のコードは createAudioFileAsync()を使用したものです。
+
+function successCallback(result) {
+    console.log("オーディオファイル準備完了： ", result);
+}
+
+function failureCallback(error) {
+    console.log("オーディオファイル生成失敗： ", error);
+}
+
+createAudioFileAsync(audioSettings, successCallback, failureCallback);
+
+// ↑をpromiseを使った場合
+createAudioFileAsync(audioSettings).then(successCallback, failureCallback);
+
+// ↑の元のコード
+const promise = createAudioFileAsync(audioSettings);
+promise.then(successCallback, failureCallback);
+
+// ↑を非同期関数呼び出し（aynchronnous function call）と呼ぶ。
+
+
+// ■ 保証
+// Promiseでは旧来のコールバックと異なり、以下が保証される
+// ・コールバックは決して現在のJavaScriptのイベントループの完了前には呼び出されない
+// ・非同期処理が完了or失敗した後にthen()によりコールバック登録した場合でも、それらのコールバックは↑の挙動になる
+// ・then()を何回呼び出して複数のコールバックを登録してもよく、追加したコールバックも追加順に独立して実行される
+
+
+// ■ Promiseチェーン
+// Promiseチェーンを作成する事で、★「複数の非同期処理を順番に実行し、前の処理が完了してからその結果を次の処理で行う」という事が出来る
+
+// 例
+const promise = doSomething();
+const promise2 = promise.then(successCallback, failureCallback);
+
+// ↑別の書き方
+const promise2 = doSomething().then(successCallback, failureCallback);
+
+// Promiseが無い時代の書き方
+doSomething(function(result) {
+    doSomethingElse(result, function(new_result) {
+        doThirdthing(new_result, function(final_result) {
+            console.log("Got the Final Result: " + final_result);
+        }, failureCallback)
+    }, failureCallback)
+}, failureCallback);
+
+// Promiseを使えば、戻り値のPromiseにコールバックを付与してチェーンとして記述出来る
+// then()関数の引数はオプションであり、catch(failureCallback)はthen(null, failureCallback)の短縮形
+doSomething().then(function(result) {
+    return doSomethingElse(result);
+})
+.then(function(new_result) {
+    return doThirdthing(new_result)
+})
+.then(function(final_result) {
+    console.log("Got the Final Result: " + final_result);
+})
+.catch(failureCallback);
+
+// ↑をアロー関数（+テンプレートリテラル）を使って記述した場合
+// コールバック関数から、処理結果を返す（次のメソッドに引数で渡す）のを忘れないようにする（そうしないと次の関数で結果を利用出来ない為）
+doSomething()
+.then(result => doSomethingElse(result))
+.then(new_result => doThirdthing(new_result))
+.then(final_result => {
+    console.log(`Got the Final Result: ${final_result}`);
+})
+.catch(failureCallback);
+
+// .cathch後のチェーン
+// catchの後にチェーンする事も可能
+new Promise((resolve, reject) => {
+    console.log("Initialize:");
+    resolve();
+})
+.then(() => {
+    throw new Error("Something failed");
+    console.log("Do that");
+})
+.catch(()=>{
+    console.log("Do that");
+})
+.then(()=>{
+    console.log("Do this whatever happend before");
+});
+
+// ↑は下記のテキストを出力する
+// Initialize
+// Do that
+// Do this whatever happend before
+// ※ Do thishは「Something failed」がスローされた場合そこで処理が止まるの出力されない
+
+
+// ■ エラーの伝播
+// Promiseチェーンではコールバック（失敗時）は1回で済む
+doSomething()
+.then(result => doSomethingElse(result))
+.then(newResult => doThirdthing(newResult))
+.then(finalResult => console.log(`Got the final result: ${finalResult}`))
+.catch(failureCallback)
+
+// 基本的に、Promiseチェーンでは例外が発生するとチェーン（連鎖）が止まり、
+// 代わりにチェーンを辿ってcatchハンドラーを探す
+// この振る舞いは↓同期的なコードと類似している
+
+try {
+    const result = syncDoSomething();
+    const newResult = syncDoSomethingElse(result);
+    const finalResult = syncDoThirdthing(newResult);
+    console.log(`Got the final result: ${finalResult}`);
+} catch(error) {
+    failureCallback(error);
+}
+
+// ES2017のaync/awaitを使えば完全に似たコードになる
+async function foo() {
+    try {
+        const result = syncDoSomething();
+        const newResult = syncDoSomethingElse(result);
+        const finalResult = syncDoThirdthing(newResult);
+        console.log(`Got the final result: ${finalResult}`);
+    } catch(error) {
+        failureCallback(error);
+    }
+}
+
+// async / awaitはPromiseの上に成り立つ、
+// 例えば上記のdoSomething()はこれまでと同じPromiseを返す関数になる。
+// 詳しくは↓
+// https://developers.google.com/web/fundamentals/primers/async-functions
+// 非同期関数 - Promise をわかりやすくする
+
+// Promiseは例外やプログラミングエラーを含むすべてのエラーを取らえる事で、
+// callback連鎖の問題を解決する
+
+
+// ■ Promiseの失敗イベント
+// Promiseが失敗するたびにglobalスコープに2つのイベントが送られる
+// ・rejectionhandled ... Promiseが失敗した後に、それがcatchハンドラー等によってハンドルされた後に送られる
+// ・unhandlerrejection ... Promiseが失敗した後に、ハンドラーが存在しない時に送られる
+
+// これらのイベントを使えば、Promiseのエラーハンドラーのフォールバックを指定出来る
+// globalにあるので、どこで発生したかに関わらず処理出来る
+
+// Node.js用のコードを書いている時に、Promiseが失敗した時の処理を↓のように書ける
+window.addEventListener("unhandledrejection", event => {
+    // ここで該当のPromiseを、event.promiseを使って、
+    // event.reasonで取得して調べる事が出来る
+    event.preventDefault(); // JavaScriptのdefaultの動作を防ぐ
+}, false);
+
+
+// ■ 古いコールバックAPIをラップするPromiseの作成
+// Promiseは、コンストラクタを使って1から作成する事も出来る
+// これは古いAPIをラップする時にのみ必要となる
+
+// 古い形式のコールバック（成功・失敗を返す、例：setTimeOut()）
+setTimeout(() => saySomething("10 second passed"), 10000);
+// ↑の場合はエラーが出ても捉えられない
+// ので、ベストプラクティスは、これらの関数を低いレベルでラップした上で直接呼ばないようにする
+
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+wait(10000).then(() => saySomething("10 seconds")).catch(failureCallback);
+
+// ■ 合成（Composition）
+
+// Promise.resolve()と、Promise.reject()はそれぞれ既にresolve()もしくはrejectされた
+// Promiseを手動で作成するショートカットで、たまに役に立つ事がある
+
+// Promise.all()とPromise.race()は同時並行で実行中の非同期処理を合成する為のツールである
+// 以下のように記載する事で、複数の処理を並行に実行し、全てが終了するのを待つ事が出来る
+
+// 複数の処理を並行に開始し、全てが終了するのを待つ
+Promise.all([func1(), func2(), func3()])
+.then(([result1, result2, result3]) => { /* result1, result2, result3が使える  */});
+
+// 逐次実行をする直列的な合成
+[func1, func2].reduce((accumulator, current_value) => accumulator.then(current_value),Promise.resolve())
+.then(result3 => { /* result3が使える */ });
+
+// 基本的に、これは非同期関数の配列を、
+// Promise.resolve().them(func1).then(func2).then(func3);と同等のPromise()チェーンへとreduce()している
+
+// このような処理は、以下のように関数型プログラミングでよくある、再利用可能な合成関数にする事が出来る
+const transformData = composeAsync(func1, func2, func3);
+const result3 = transformData(data);
+
+// ES2017では、直列的な合成はもう少し簡単に書ける
+let result;
+for (const f of [func1, func2, func3]) {
+    result = await f(result);
+}
+// 最終的な結果（result3）が使える
+
+
+// ■ タイミング
+// 想定外な事態とならない用に、例え既にresolveされたPromiseであっても、thenに渡される関数が同期的に呼ばれる事は無い
+Promise.resolve().then(()=>console.log(2));
+console.log(1); // 1, 2
+
+// 渡された関数はすぐに実行されるのではなく、マイクロタスクキューに入る
+// 現在のイベントループの終わりにこのキューは空にされ、この関数が実行される(＝かなり早い段階)
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+wait().then(()=>console.log(4));
+Promise.resolve().then(()=>console.log(2)).then(()=>console.log(3));
+console.log(1); // 1 ,2, 3, 4
+
+// ■ ネスト
+// 単純なPromiseチェーンならば、ネストは不用意な合成の結果生まれるものなので、
+// ネストせず、平らにしておくのがベストである
+
+// ネストするのは、catchステートメントのスコープを制限する為の制御構造である
+// （正確にはcatchはそのネストされたスコープ内しかキャッチしない）
+
+doSomethingCritical()
+.then(result => doSomethingOptical(result)
+    .then(opticalResult => doSomethingExtra())
+    .catch(e => {}))
+.then(() => moreCriticalStuff())
+.catch(e => console.log("Critical failure: " + e.message));
+// 内側のcatchは、doSomethingOpticalとdoSomethingExtraからの失敗のみをキャッチする（キャッチした後はmoreCriticalStuffへと処理が続く）
+// doSomethingCriticalが失敗した場合、エラーは最後のcatchによってのみキャッチされる
+
+// ■ よくある間違い
+// Promiseチェーンを合成する時に、以下の様な間違いに気を付ける必要がある
+
+// 間違いを3つ探して下さい
+
+doSomething().then(function(result) {
+    doSomethingElse(result)
+    .then(newResult => doThirdthing(newResult));    
+}).then(() => doFourthThing());
+
+doSomething().then(function(result) {
+    doSomethingElse(result) // 内側のチェーンがPromiseを返していない+不必要なネスト
+    .then(newResult => doThirdthing(newResult));    
+}).then(() => doFourthThing());
+// チェーンの最後をcatch()で終わらせていない
+
+//（１）チェーンを適切に構成出来ていない
+// doFourthThing()は、doSomethingElse()やdoSomethingElse()の終了を待たずに並列に実行されてしまう
+
+//（２）不必要にネストしている
+// 内側のエラーハンドラーが制限される
+// 参考：
+// https://stackoverflow.com/questions/23803743/what-is-the-explicit-promise-construction-antipattern-and-how-do-i-avoid-it
+// What is the explicit promise construction antipattern and how do I avoid it?
+// ↓
+// Promise Anti-patterns
+// http://taoofcode.net/promise-anti-patterns/
+
+// （３）チェーンをcacch()で終わらせていない
+// ＝失敗がキャッチされない
+
+// ★イディオムとして、★Promiseチェーンは常にreturnするか、catchで終わらせ、新しいPromiseを得るたびにすぐreturnしてチェーンを平らにする
+doSomething()
+.then(function(result) {
+    return doSomethingElse(result);
+})
+.then(newResult => doThirdthing(newResult))
+.then(()=>doFourthThing())
+.catch(error => console.log(error));
+
+// ※ () => xは、() => { return x }の短縮形である事に注意、※一行の時は{}を省略可、returnのみ時はreturn表記を省略可
