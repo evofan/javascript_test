f5691ba148ce6c43fd428f4840f144de48a333ae
evofan <evofan@outlook.jp>
2019-10-17 22:45:15 +0900

    add for...of, Map()

---
+++ src/for_of.js
@@ -0,0 +1,263 @@
+﻿// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Statements/for...of
+// for...of
+
+// for...of 文は、反復可能オブジェクト、たとえば組込みの String, Array, 配列風オブジェクト (例えば arguments や NodeList), TypedArray, Map, Set,
+// およびユーザー定義の反復可能オブジェクトなどに対して、反復的な処理をするループを作成します。
+// これはオブジェクトのそれぞれの識別可能なプロパティの値に対して、実行される文を表す独自の反復フックを呼び出します。
+const array1 = ["a", "b", "c", "d"];
+
+for (let element of array1) {
+  console.log(element);
+}
+// a
+// b
+// c
+// d
+
+// ■ Syntax
+
+// for ( variable of iterableObj) {
+//    statement
+// }
+
+// variable ... 反復処理の各回において、異なるプロパティの値がvariableに割り当てられる。const, let, varで宣言可能
+// iterable ... 反復処理が行われる、反復可能（イテレーブル）なプロパティを持つオブジェクト
+
+// ■ 例
+
+// □ Arrayでの反復処理
+const iterable1 = [10, 20, 30];
+for (const val of iterable1) {
+  console.log(val);
+}
+// 10
+// 20
+// 30
+
+// 変数宣言にletの場合
+for (let val of iterable1) {
+  val += 1;
+  console.log(val);
+}
+// 11、ブロック内で変数が再割り当てされる
+// 21
+// 31
+
+// □ Stringでの反復処理
+const iterable2 = "あいうえお";
+for (let val of iterable2) {
+  console.log(val);
+}
+// あ
+// い
+// う
+// え
+// お
+
+// □ TypedArrayでの反復処理
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/TypedArray
+// TypedArray ... TypedArrayオブジェクトは背後にあるバイナリーデータバッファーの、配列状のビューを表します。※8ビット長・2の補数方式の符号付き整数値等
+const iterable3 = new Uint8Array([0x00, 0xff]);
+for (let val of iterable3) {
+  console.log(val);
+}
+// 0
+// 255
+
+// □ Mapでの反復処理
+const iterable4 = new Map([["a", 1], ["b", 2], ["c", 3]]);
+for (let val of iterable4) {
+  console.log(val);
+}
+// ["a", 1]
+// ["b", 2]
+// ["c", 3]
+// 2つの要素の内の後ろの値を取得（{"a":1}ならkey: valueだけど、["a",1]でもkey, value？）
+for (let [a, b] of iterable4) {
+  console.log(b);
+}
+// 1
+// 2
+// 3
+
+// □ setでの反復処理
+const iterable5 = new Set([1, 1, 2, 2, 3, 3]);
+for (let val of iterable5) {
+  console.log(val);
+}
+// 1
+// 2
+// 3
+
+// □ argumentオブジェクトでの反復処理
+// argumentオブジェクトで反復処理をすると、ある関数に全ての引数を渡す事が出来る
+(function() {
+  for (let el of arguments) {
+    console.log(el);
+  }
+})(1, 2, 3, 4);
+// 1
+// 2
+// 3
+// 4
+
+// □ DOMコレクションでの反復処理
+// NodeListのようなDOMコレクションでの反復処理
+// 注: これは以下のものに対応しているプラットフォームでのみ動作します。
+// implemented NodeList.prototype[Symbol.iterator]
+/*
+const articleparagraphs = document.querySelectorAll("article > p");
+for (let paragraph of articleparagraphs) {
+    paragraph.classList.add("read");
+}
+*/
+
+console.log("");
+
+// □ 反復処理の終了
+// for...ofループ内では、break, throw, returnを発生させる事で反復処理を中断させる事が出来る（その場合、反復子は閉じられる→次の処理に行く）
+function* foo() {
+  yield 1;
+  yield 2;
+  yield 3;
+}
+
+for (let o of foo()) {
+  console.log(o); // 1（※のみ実行されて表示）
+  break; // 反復子を閉じ、ループ外の実行が継続される
+}
+console.log("done"); // done
+
+console.log("");
+
+// □ ジェネレーターでの反復処理
+// ジェネレーター（反復可能オブジェクトを生成する関数）で反復処理をする事が出来る
+function* fibonacci() {
+  let [prev, curr] = [0, 1];
+  while (true) {
+    [prev, curr] = [curr, prev + curr];
+    yield curr;
+  }
+}
+for (let num of fibonacci()) {
+  console.log(num);
+  // 1000で繰り返しを終了する
+  if (num > 1000) {
+    break;
+  }
+}
+// 1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597
+// ジェネレーターを（for...ofで）再利用してはならない。ループを一旦抜けると閉じられ、そこで繰り返してもそれ以上の結果は算出されないので
+/*
+for (let num of fibonacci()) {
+  console.log("再利用中... ", num); // MDNだと「Nerver Called」だけど実際は呼ばれる？
+}
+*/
+// 再利用中...  7.283601309201629e+306
+// 再利用中...  1.1785114478791467e+307
+// 再利用中...  1.9068715787993096e+307
+// 再利用中...  3.085383026678456e+307
+// 再利用中...  4.992254605477766e+307
+// 再利用中...  8.077637632156222e+307
+// 再利用中...  1.3069892237633987e+308
+// 再利用中...  Infinity
+
+console.log("");
+
+// □ イテレーターの同期
+let range = {
+  from: 1,
+  to: 5,
+
+  [Symbol.iterator]() {
+    return {
+      current: this.from,
+      last: this.to,
+
+      next() {
+        if (this.current <= this.last) {
+          return { done: false, value: this.current++ };
+        } else {
+          return { done: true };
+        }
+      }
+    };
+  }
+};
+for (let val of range) {
+  console.log(val);
+  
+}
+// 1
+// 2
+// 3
+// 4
+// 5
+
+console.log("");
+
+// □ その他の反復可能オブジェクトでの反復処理
+// 明示的にiterableプロトコルを実装しているオブジェクトで反復処理が出来る
+const iterable6 = {
+  [Symbol.iterator]() {
+    return {
+      i: 0,
+      next() {
+        if (this.i < 3) {
+          return { value: this.i++, done: false };
+        }
+        return { value: undefined, done: true };
+      }
+    };
+  }
+};
+for (let val of iterable6) {
+  console.log(val);
+}
+// 0
+// 1
+// 2
+
+console.log("");
+
+// ■ for...ofとfor...inの違い
+// for...inは、オブジェクトの全ての列挙可能なプロパティに対し、順序不同で繰り返し処理を行う
+// for...ofは、反復可能なオブジェクトが定義した順序で値を反復処理する
+// ↓違い
+Object.prototype.objCustom = function() {}; // 全てのObjcetオブジェクトがobjCustomを継承
+Array.prototype.arrCustom = function() {}; // 全てのArrayオブジェクトがarrCustomを継承
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Guide/Inheritance_and_the_prototype_chain
+// 継承とプロトタイプチェーン
+
+const iterable7 = [3, 5, 7];
+iterable7.foo = "hello!";
+
+for (let i in iterable7) {
+  console.log(i);
+}
+// 0
+// 1
+// 2
+// foo
+// arrCustom
+// objCustom
+// ※配列の要素である3,5,7やfooは列挙可能なプロパティで無い事に注意＝★プロパティでは無く値
+
+for (let i in iterable7) {
+  if (iterable7.hasOwnProperty(i)) {
+    // 継承したものは除く＝自身のプロパティ、fooは自分の、customは継承されたもの
+    console.log(i);
+  }
+}
+// 0
+// 1
+// 2
+// foo
+
+for (let i of iterable7) {
+  console.log(i);
+}
+// 3
+// 5
+// 7
+// ※ofは反復可能なオブジェクトとして定義している順序で値を反復処理して出力する＝オブジェクトの要素の3、5、7が表示される＝プロパティは表示されない
--- src/map.js
+++ src/map.js
@@ -1,130 +1,161 @@
-﻿// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/map
-// map() メソッドは、与えられた「★関数を配列のすべての要素に対して呼び出し」、その結果からなる「★新しい配列を生成」します。
+﻿// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Map
+// Map
+// Mapオブジェクトは単純なキー/値 写像（マップ）です。キーあるいは値として任意の値（オブジェクト、プリミティブ値とも）を使う事が出来ます。
 
-let array1 = [1, 4, 9, 16];
-const map1 = array1.map(x => x * 2);
-console.log("map1: ", map1); // [2, 8, 18, 32]
+// https://linky-juku.com/set-and-mapping/
+// 写像とは（←この前に「集合」について理解しておく事）
+// 写像とは、ある集合の要素から、他の集合の要素とを対応させること、と言えます。
+// ★「全単射」... 2つの集合の要素が1:1で対応しているもの
+// ★「全射」... P→Qが可能でも、Q→Pが不可能な場合（↑サイトの図で言うと身長の所）
+// 　　　　　　　P→Qの写像は成り立つが、QからPへの写像（逆写像）は成立しない時が全射
+// ★「単射」... P→Qの要素が対応していても、QからPへの対応が無い場合（↑サイトの図で言うと165cm）は単射
 
-// [Syntax]
-// let new_array = arr.map(function callback(currentValue, [index], [array]) {
-//    // 新しい配列の要素を返す
-// }, [thisArg]);
+// 写像の意味を捉えておく事で、ベクトル空間。行列を理解するのに役に立つ
 
-// [引数]
-// callback() ... 新しい配列の要素を生成する為の関数、次の3つの引数を取る
-//  currentValue ... 現在処理中の要素の値
-//  index ... （オプション）現在処理中の要素の配列内におけるインデックス
-//  array ... （オプション）mapが実行されている配列
-//
-// thisArg ... （オプション）callback()を実行する時にthisとして扱う値
+// ■ Syntax
+// new Map([iterable])
 
-// [返り値]
-// 与えられた関数を配列内の全ての要素に対して実行し、その結果からなる新しい配列
+// □ 引数
+// iterable ... 要素が「キー・値」の対（2要素）であるArray又は他の反復処理可能（イテレーブル）なオブジェクト
 
-// [解説]
-// mapは与えられたcallback関数を配列の順番通りに、各要素に対して一度ずつ呼び出し、
-// その結果から新しい配列を作成する
+// ■ 解説
+// Mapオブジェクトは、その要素について、挿入順で反復処理を行う事が出来る
+// for～ofループを使う事で、[キー（key）, 値（value）]の配列を返す
 
-// callback()は値が代入されているが配列のインデックスに対してのみ呼び出される
-// （undefinedが代入されているものは含む、値が代入されてないものや、削除されたインデックスは含まない）
+// ObjectのMap、特にDictionary？のMapはObjectの挿入順にのみマップされる
 
-// thisArgが与えられた場合、callback()の呼び出しの度にそれがthisとして使われる
-// （引数が省略された場合は、thisはundfinedになる）
-// 最終的には、関数における通常のthisを決定するルール（https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/this）によって決まる
+// □ キーの等価性
+// キーの等価性は「same-value」アルゴリズムに基づく
+// （NaN !== NaNであるにも関わらず、NaNはNaNと同一とみなされ、他の全ての値は===演算子の動作に従って等しいとみなされる）
 
-// map()は呼び出された元の配列を変更しない
+// □ オブジェクトとマップの比較
+// ObjectsとMapは、両者とも値へのキーを設定したり、それらの値を取り出したり、キーを削除したり、
+// また何かがあるキーに格納されているかを判定したりする事が出来る、という点で似ている
 
-// map()が呼び出された後に元の配列が変更されても影響を受けない
+// このため、歴史的にObjectはMapとして使われてきた。
+// しかし、Mapの使用を望ましくするObjectとMapの重要な違いが存在する
 
-// map()が呼び出された配列がまばらである場合、結果の配列も同じインデックスを空白に保つ
+// ・ObjectのキーはStringsとSymbolsであるが、Mapは★「任意の値がキー」となり得る
+// ・Mapの大きさはsizeプロパティで簡単に得る事出来るが、Objectは手動で保つ？必要がある
+// ・Mapはiterableで直接反復処理を実行出来るが、Objectは何らかの方法でキーを取得しそのキーを元に反復処理をする必要がある
+// ・Objectはプロトタイプを持つため、既定のキーがマップ中に存在する。
+//  （Object.create(null)で回避出来るが、推奨はしない）
+// ・Mapは頻繁に要素を追加したり削除したりする場合、Objectに比べてパフォーマンスが良い場合？がある
 
-// [例]
-// ■ 数値の配列を平方根の配列にマッチングする
-let numbers = [1, 4, 9];
-let roots = numbers.map(function(num) {
-  return Math.sqrt(num);
-});
-console.log("numbers: ", numbers); // numbers: [1, 4, 9]
-console.log("roots: ", roots); // roots: [1, 2, 3]
+// ■ プロパティ
+// Map.length ... lengthプロパティはあるが、値は常に0である。
+// get Map[@@species] ... 派生クラスを生成するためのコンストラクタ関数
+// Map.prototype ... Mapコンストラクタのプロトタイプを表す。全てのMapオブジェクトに追加のプロパティを定義出来る。
 
-// ■ map()を使用して配列内のオブジェクトを再フォーマット
-let kvArray = [
-  { key: 1, value: 10 },
-  { key: 2, value: 20 },
-  { key: 3, value: 30 }
-];
-let reformattedArray = kvArray.map(obj => {
-  let rObj = {};
-  rObj[obj.key] = obj.value;
-  return rObj;
-});
-console.log("reformattedArray: ", reformattedArray);
-// reformattedArray: [
-//  {1: 10}
-//  {2: 20}
-//  {3: 30}
-// ]
+// ■ マップインスタンス
+// 全てのMapインスタンスは、Map.prototypeを継承する
 
-// ■ 引数を含む関数を利用して、数値配列をマッピングする
-// 1つの引数を必要とする関数を使用する時に、map()がどのように動作するかを示している
-let numbers2 = [1, 4, 9, 16];
-let roots2 = numbers2.map(function(elment) {
-  return Math.sqrt(elment);
-});
-console.log("roots2: ", roots2); // roots2: [1, 2, 3, 4]
+// □ プロパティ
+// Map.prototype.constructor ... インスタンスのプロトタイプを生成する関数を返す（defaultはMap関数）
+// Map.prototype.size ... Mapオブジェクト内のkey：valueペアの数を返す
 
-// ■ 汎用的なmap()の使用
-let map2 = Array.prototype.map;
-let word_code = map2.call('Hello World', function(x) {
-    return x.charCodeAt(0); // String.prototype.charCodeAt() ... 与えられたindexに位置する文字のUTF-16コード（0-65535）を表す
-});
-console.log("word_code: ", word_code); // word_code: [72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100]
+// □ メソッド
+// Map.prototype.clear() ... Mapオブジェクトから全てのkey：valueペアを削除する
+// Map.prototype.delete(key) ... keyに関連した値を削除し、Map.prototype.has(value)が以前返した値を返す（has(value)はその後falseを返す）
+// Map.prototype.entries() ... Mapオブジェクト内の要素に対し、挿入順に[key, value]の配列を含む★新しいiteratorオブジェクトを返す
+// Map.prototype.forEach(callback, [thisArg]) ... Mapオブジェクト内のkey：valueペアに対して挿入順に、1回コールバック関数を呼び出して適用する
+//                                                 thisArgが渡された場合、callbackに対するthisの値をして使用される
+// Map.prototype.get(key) ... keyに関連した値を返す（無い場合はundefinedが返る）
+// Map.prototype.has(key) ... Mapオブジェクト内に指定したkey要素があるかをbool値で返す
+// Map.prototype.keys() ... Mapオブジェクト内の要素に対し、挿入順にkeysを含む新しいiteratorオブジェクトを返す
+// Map.prototype.set(key, value) ... Mapオブジェクト内にkey: vauesペアの値を設定する
+// Map.prototype.values() ... Mapオブジェクト内の要素に対し、挿入順にvaluesを含む新しいiteratorオブジェクトを返す
+// Map.prototype[@@iterator]() ... Mapオブジェクト内の要素に対し、挿入順に[key, value]の配列を含む新しいiteratorオブジェクトを返す
 
-// ■ 汎用的なmap()の使用：querySelectorAll
-let elems = document.querySelectorAll("select option:checked");
-let value = Array.prototype.map.call(elems, function(obj) {
-    return obj.value;
-});
+// ■ 例
 
-// ■ トリッキーな使用例
-// コールバック関数は第一引数だけを指定するケースが殆どだが、意図しないケースがあるので注意
-let num2 = ["1", "2", "3"].map(parseInt);
-console.log("num2: ", num2); // num2:  (3) [1, NaN, NaN]
-// ↓の処理をしているので
-// parseInt(string, radix) -> map(parseInt(value, index))
-/*
-    first iteration (index is 0): parseInt("1", 0); // 1
-    second iteration (index is 1): parseInt("2", 1); // NaN
-    third iteration (index is 2): parseInt("3", 2); // NaN
-*/
+// □ Mapオブジェクトの使用
+let myMap = new Map();
+let keyString = "文字列";
+let keyObj = {};
+let keyFunc = function() {};
 
-// 解決策
-function returnInt(element) {
-    return parseInt(element, 10);
+// 値を設定する
+myMap.set(keyString, "'String'と関連付けられた値");
+myMap.set(keyObj, "'keyObj'と関連付けられた値");
+myMap.set(keyFunc, "'keyFunc'と関連付けられた値");
+
+console.log(myMap); // Map(3) {"文字列" => "'String'と関連付けられた値", {…} => "'keyObj'と関連付けられた値", ƒ => "'keyFunc'と関連付けられた値"}
+console.log(myMap.size); // 3
+
+// 値を取得する
+console.log(myMap.get(keyString)); // 'String'と関連付けられた値
+console.log(myMap.get(keyObj)); // 'keyObj'と関連付けられた値
+console.log(myMap.get(keyFunc)); // 'keyFunc'と関連付けられた値
+
+console.log(myMap.get("文字列")); // 'String'と関連付けられた値、※keyString === '文字列' であるため
+console.log(myMap.get({})); // undefined、※keyObj !== {}であるため
+console.log(myMap.get(function() {})); // undefined、※keyFunc !== function() {}であるため
+
+// □ MapのキーとしてのNaNの使用
+// NaNもまたキーとして使用する事が出来る。全てNaNは自身と等しくない（NaN !== NaN）にも関わらず、↓の例は動作する。これはNaNが互いに区別出来ない為
+let myMap2 = new Map();
+myMap2.set(NaN, "Not a Number");
+
+console.log(myMap2.get(NaN)); // Not a Number
+
+let otherNaN = Number("foo");
+console.log(myMap2.get(otherNaN)); // Not a Number
+
+// □ for..ofを使用するMapの反復処理
+// Mapはfor...ofを用いて反復処理を行う事が出来る
+let myMap3 = new Map();
+myMap3.set(0, "zero");
+myMap3.set(1, "one");
+for (let [key, value] of myMap3) {
+  console.log(key + " : " + value);
 }
+// 0 : zero
+// 1 : one
 
-let num3 = ["1", "2", "3"].map(returnInt);
-console.log("mum3: ", num3); // mum3: [1, 2, 3]
+for (let key of myMap3.keys()) {
+  console.log(key);
+}
+// 0
+// 1
 
-let num4 = ["1", "2", "3"].map(str => parseInt(str));
-console.log("num4: ", num4); // num4: [1, 2, 3]
+for (let value of myMap3.values()) {
+  console.log(value);
+}
+// zero
+// one
 
-let num5 = ["1", "2", "3"].map(Number);
-console.log("num5: ", num5); // num5: [1, 2, 3]
+for (let [key, value] of myMap3.entries()) {
+  console.log(key + " : " + value);
+}
+// 0 : zero
+// 1 : one
 
-// parseInt()と違って、Number()はfloat又は解決した指数表表現を返す
-let num6 = ["1.1", "2.2e2", "3e300"].map(Number);
-console.log("num6: ", num6); // num6: [1.1, 220, 3e+300]
-
-// ■ undefinedを持つ配列のマッピング
-let numbers3 = [1, 2, 3, 4];
-let filteredNumbers = numbers3.map(function(num, index) {
-    if (index < 2) {
-        return num;
-    }
-    return ""; // undefined以外を指定したい場合
+// □ forEach()でMapsを反復処理
+// MapはforEach()を使用して反復出来る
+myMap3.forEach((value, key) => {
+  console.log(key + " : " + value);
 });
-console.log("filteredNumbers: ", filteredNumbers); // filteredNumbers: [1, 2, undefined, undefined] // なにも指定しないと該当しない場合はundefinedが返る
+// 0 : zero
+// 1 : one
 
-// ポリフィル
-// 省略
+// □ Arrayオブジェクトとの関係
+let kvArray = [["キー1", "値1"], ["キー2", "値2"]];
+console.log(kvArray); //  [Array(2), Array(2)]
+
+// 通常のMap()コンストラクタを使って、キー・値の二次元配列をマップに変換する（配列→Mapオブジェクト）
+let myMap4 = new Map(kvArray);
+console.log(myMap4); // Map(2) {"キー1" => "値1", "キー2" => "値2"}
+
+// 特定のキーを指定してその値を取得
+console.log(myMap4.get("キー1")); // 値1
+
+// （元：展開演算子？←spread構文（...）が出て来る）
+// Array.from()を使って、マップをキー・値の二次元配列に変換する（↑と逆にMapオブジェクト→配列）
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/from
+// Array.from() ... Array.from() メソッドは、配列風オブジェクトや反復可能オブジェクトから、新しい、浅いコピーの Array インスタンスを生成します。
+console.log(Array.from(myMap4)); // (2) [Array(2), Array(2)]
+
+// あるいはArray.from()を、キー（又は値）のイテレーターに使って、キー（又は値）のみの配列を得る
+console.log(Array.from(myMap4.keys())); // (2) ["キー1", "キー2"]
+console.log(Array.from(myMap4.values())); // (2) ["値1", "値2"]s
